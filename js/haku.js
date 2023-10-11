6a6e9c05f960979047598c69790072605af9cc4d
Veera Virtanen <veeravirtanen@Veeras-MacBook-Air-2.local>
2023-10-11 21:07:48 +0300

    Valmis nettisivu

---
+++ js/haku.js
@@ -0,0 +1,130 @@
+const hakemisto = [
+    {
+        sisalto: `Mitä tanssi on? 
+        Tanssi on sekä taidetta että liikuntaa. Yleinen määritelmä tanssista on se, että tanssiessa liikutetaan
+        kehoa
+        tiettyyn rytmiin yleensä musiikin tahtiin. Esittävänä taiteena tanssi on kaunista katsottavaa, mutta se
+        on
+        myös
+        monipuolista liikuntaa. Tanssi, kuten kaikki muutkin liikuntalajit edistävät ihmisen fyysistä
+        ja
+        psyykkistä
+        terveyttä. Tanssilla on kuitenkin syvempi vaikutus ihmiseen. Jokaisella ihmisellä on oma uniikki
+        tanssityylinsä, joka on osa tanssijan identiteettiä. Tanssiessa ollaan läsnä omassa kehossa ja pystytään
+        irtaantumaan muusta maailmasta.`,
+        url: "index.html",
+        nimi: "Etusivu"
+    },
+    {
+        sisalto: `Tanssi osana elämää
+        Tanssiessa oppii tuntemaan omaa kehoaan ja omaa liikekieltään.
+        Sen kautta pystytään välittämään tunteita ja kertoa tarinoita. 
+        Aina tanssilla ei kuitenkaan tarvitse olla mitään sanomaa. 
+        Joskus vain liikkuminen mieluiseen tapaan tekee hyvää.
+        Tanssia käytetään jopa terapiamuotona. Joitain asioita on vaikea sanoittaa, jolloin hyvä tapa ilmaista niitä on kehon liikkeillä.
+        Mielenterveyden lisäksi tanssi tietenkin myös edistää fyysistä terveyttä.
+        Tanssi onkin erinomainen liikuntalaji, koska se on niin monipuolista.
+        Kehoa kannateltaessa tanssiessa oikealla tavalla tulee käytettyä syvempiä lihaksia varsinkin keskivartalossa. 
+        Tanssi kehittää lihasvoimaa, kestävyyttä sekä liikkuvuutta ja tasapainoa.`,
+        url: "merkitys.html",
+        nimi: "Tanssin merkitys"
+    },
+    {
+        sisalto: `Baletti Suuri osa nykypäivän tanssilajeista on kehittyneet baletin tekniikan pohjalta.
+        Klassisessa baletissa tärkeää on esteettisyys ja itsensä esittäminen mahdollisimman sirona ja
+        kevyenä. Moderni tanssi  Moderni tanssi syntyi baletin pohjalta ja vuosien varrella se on kehittynyt erilaisiin suuntiin.
+        Perusajatus kaikissa on kuitenkin sama: liikkeiden vapaus ja sitä kautta syvien tunteiden ilmaiseminen.`,
+        url: "tanssilajit.html",
+        nimi: "Tanssilajit"
+    },
+    {
+        sisalto: `Baletti - Vanhin länsimaisen taidetanssin laji Ensimmäisen kerran balettia on tanssinut hoviväki Italiassa jo 1500-luvulla. Hovibaletti levisi
+        Ranskaan ja siellä siitä alettiin kehittämään omaa taidemuotoansa.
+        Ranskan aurinkokuningas Ludvig XIV tanssi niihin aikoihin itsekin balettia. Tällöin kehitettiin mm.
+        baletin jalkojen viisi perusasentoa, joita käytetään yhä nykypäivänäkin.
+        Aluksi baletti oli vain oopperan laajennus, mutta 1713 Pariisin Oopperassa aloitettiin uudenlainen
+        tanssikoulutus balettitanssijoille.
+        1700-luvulla baletista muodostui itsenäinen lajinsa. 1830-luvulta alkaen baletissa
+        kukoisti romantiikan vaihe. Romantiikan tyylisuunnalle tyypilliset piirteet tuli tällöin esille myös
+        baleteissa.
+        Tällaisia olivat mm. yliluonnollisuus ja suuret tunteet. Aikaisemmin miestanssijat eli ballerinot
+        olivat olleet suuremmassa osassa baletissa, mutta tästä alkoi naistanssijoiden eli ballerinojen
+        kukoistus.
+        Yleensä naistanssijoiden käyttämä baletin tunnusmerkki asustus eli tutu tuli käyttöön ensimmäisen
+        kerran 1832 La Sylphide-baletissa.
+        Ennen tätä ballerinat olivat käyttäneet pidempiä hameita, mutta tämä uusi lyhyempi tutu sai jalkojen
+        liikkeet näkymään paremmin. Tällöin balleriinan tekniset taidot tulivat paremmin esille.
+        En pointe eli varpaiden kärjillä tanssiminen otettiin käyttöön ensimmäistä kertaa romanttisen
+        baletin aikana. Kärkitossuja käyttää yleensä vain naistanssijat. Niiden avulla tanssi saadaan
+        näyttämään kevyeltä, sirolta ja kauniilta.
+        Tarkoitus on saada tanssi näyttämään mahdollisimman helpolta, vaikka todellisuudessa se ei sitä ole.
+        Kärkitossujen käytöstä tulee usein rakkoja ja niillä tanssiminen saattaa olla välillä kivuliasta,
+        vaikka varpaiden kärjissä käytetäänkin erilaisia pehmusteita.
+        Romantiikan ajan jälkeen baletin painopiste siirtyi Venäjälle. Venäläinen Pyotr Tchaikovskyn sävelsi
+        useita maailman tunnetuimpia baletteja, kuten Joutsenlampi ja Pähkinänsärkijä. Venäjällä kehittyi
+        omanlainen balettityylinsä.
+        Nykyään on erilaisia baletin tekniikkatyylejä esimerkiksi venäläinen
+        Vaganova tekniikka ja perinteinen ranskalainen tekniikka.
+        Niiden luonteiden välillä on joitakin eroja, mutta perusteet ja termit ovat samoja.
+        Tunnetuimpia baletteja ovat Joutsenlampi, Pähkinänsärkijä, Coppelia, Prinsessa Ruusunen, La
+        bayadère, Don Quixote, Giselle sekä Romeo ja Julia.`,
+        url: "baletti.html",
+        nimi: "Baletti"
+    },
+    {
+        sisalto: `Moderni tanssi - Rikotaan baletin ihanteita Kuten monet muutkin tanssilajit, myös moderni tanssi kehittyi baletin pohjalta 1900-luvulla. 
+        Sen ideana oli rikkoa baletin ihanteita, rajoja ja tekniikkaa, jotta tanssijat pystyisivät liikkumaan ja ilmaisemaan tunteita vapaammin. 
+        Modernin tanssin historian tunnetuin henkilö on amerikkalainen Isadora Duncan. Hän järkytti yleisöä tanssimalla paljain jaloin. 
+        Amerikkalainen Martha Graham kehitti modernille tanssille ensimmäistä omanlaistaan tekniikka, jossa muun muassa lattiatyöskentelyllä on merkittävä rooli. 
+        Balettiin verrattuna se on paljon vapaampaa eikä tekniikka rajoita tanssimista, vaan voidaan keskittyä ilmaisuun ja tarinankerrontaan.
+        Grahamin tanssioppilas Merce Cunninghamin kehitti 1900-luvun loppupuolella modernin tanssin pohjalta nykytanssin. Nykytanssissa keskitytään enemmän itse liikkeeseen ja se on ennalta-arvaamattomampaa. 
+        Improvisaatio on merkittävässä osassa varsinkin modernissa tanssissa. Improvisaatio toimii hyvänä välineenä koreografioiden ideoinnissa. 
+        Improvisaatio poistaa tanssijalta kaikki rajoitteet, jolloin oma ilmaisu ja kehonkieli korostuvat.`,
+        url: "nykytanssi.html",
+        nimi: "Moderni tanssi"
+    }
+]
+
+const hakusana = decodeURIComponent(window.location.hash.substring(1));
+const hakutulokset = document.getElementById("hakutulokset");
+hae(hakusana);
+
+function hae(sana) {
+    document.getElementById("hakusana").textContent = "Hakutulokset sanalle: " + sana;
+    for (let i = 0; i < hakemisto.length; i++) {
+        const sivu = hakemisto[i];
+        const sisalto = sivu.sisalto.toLowerCase();
+        let indeksit = [];
+        let j = 0;
+        while (true) {
+            j = sisalto.indexOf(sana.toLowerCase(), j);
+            if (j === -1) {
+                break
+            }
+            indeksit.push(j);
+            j += sana.length;
+        }
+
+        for (let k = 0; k < indeksit.length; k++) {
+            const indeksi = indeksit[k];
+            const a = document.createElement("a");
+            a.href = sivu.url;
+            let loppu = Math.min(sivu.sisalto.length, indeksi + sana.length + 40);
+            for (; loppu < sivu.sisalto.length; loppu++) {
+                if (sivu.sisalto[loppu] === " ") {
+                    break;
+                }
+            }
+            let alku = Math.max(0, indeksi - 40);
+            for (; alku >= 0; alku--) {
+                if (sivu.sisalto[alku] === " ") {
+                    break;
+                }
+            }
+            a.innerHTML = sivu.nimi + ": "+ sivu.sisalto.substring(alku, indeksi) + "<b>" + sana + "</b>" + sivu.sisalto.substring(indeksi + sana.length, loppu) + "...";
+            hakutulokset.append(a);
+        }
+
+    }
+
+}
--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
 const valikkoNappi = document.getElementById("dropdown-button");
 const valikko = document.getElementById("links");
 
+const haku = document.getElementById("search-field");
+const hakuNappi = document.getElementById("search-button");
+
 valikkoNappi.addEventListener("click", avaaValikko);
+
+haku.addEventListener("keydown", tarkistaPainallus);
+hakuNappi.addEventListener("click", haeTuloksia);
 
 function avaaValikko() {
     valikko.classList.toggle("show");
+}
+
+function tarkistaPainallus(event) {
+    if (event.key === "Enter") {
+        haeTuloksia();
+    }
+}
+
+function haeTuloksia() {
+    window.location.href = "hakutulokset.html#" + encodeURIComponent(haku.value);
+    if (window.location.pathname.endsWith("/hakutulokset.html")) {
+        window.location.reload();
+    }
 }
